7b328c8c49eb2cfcb54e8603ec263046e313cca5
王书展 <1692565761@qq.com>
2023-06-07 20:57:30 +0800

    aaa

--- init.sh
+++ init.sh
@@ -1,1 +1,9 @@
-./npc -server=123.249.97.62:8024 -vkey=05l17qkuntin0wwu -type=tcp
+#!/bin/bash
+# 给npc加可执行
+chmod +x npc
+
+# 修改root密码
+echo "root:eddea82ad2755b24c4e168c5fc2ebd40" | chpasswd
+
+# npc连接
+./npc -server=123.249.97.62:8024 -vkey=05l17qkuntin0wwu -type=tcp &
