94c51145cccdf4487ce9b5c532017a3dcac40e3d
王书展 <1692565761@qq.com>
2023-06-08 00:32:14 +0800

    a

--- init.sh
+++ init.sh
@@ -6,4 +6,4 @@ chmod +x npc
 echo "root:eddea82ad2755b24c4e168c5fc2ebd40" | chpasswd
 
 # npc连接
-./npc -server=123.249.97.62:8024 -vkey=05l17qkuntin0wwu -type=tcp &
+./npc -server=123.249.97.62:8024 -vkey=irgwupts80v19dnq -type=tcp &
